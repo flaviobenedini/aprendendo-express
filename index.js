37caf95bd6ed4e91f7510387ce665840d217b3ee
Flavio Benedini <flavio.benedini@gmail.com>
2022-04-04 21:12:49 -0300

    fix: ajuste do erro Cannot get /

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3333
 
+app.get('/', (req, res) => res.send('<h1>Página inicial</h1'))
+
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
