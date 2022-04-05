8867607080066cd43856c7fa085ca73cafe692d4
Flavio Benedini <flavio.benedini@gmail.com>
2022-04-04 21:45:00 -0300

    feat: enviando HTML na resposta do servidor

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3333
 
-app.get('/', (req, res) => res.send('<h1>Página inicial</h1'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'views/index.html')))
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
